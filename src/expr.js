1963c66d76ee7da4a35a11578079431295417265
David Li <li.davidm96@gmail.com>
2017-12-07 21:39:57 -0500

    Flatten nodes in store

--- src/expr.js
+++ src/expr.js
@@ -1,11 +1,11 @@
 export function number(value) {
-    return { kind: "number", value: value };
+    return { type: "number", value: value };
 }
 
 export function missing() {
-    return { kind: "missing" };
+    return { type: "missing" };
 }
 
 export function add(expr1, expr2) {
-    return { kind: "add", left: expr1, right: expr2 };
+    return { type: "add", left: expr1, right: expr2 };
 }
--- src/reducer.js
+++ src/reducer.js
@@ -1,18 +1,59 @@
 import * as action from "./action";
 
 const initialState = {
+    nodes: {},
     goal: [],
     board: [],
     toolbox: [],
 };
 
+let idCounter = 0;
+
+function flatten(expr) {
+    switch (expr.type) {
+    case "number":
+    case "missing": {
+        expr.id = idCounter++;
+        return [expr];
+    }
+    case "add": {
+        expr.id = idCounter++;
+        const result = [expr].concat(flatten(expr.left)).concat(flatten(expr.right));
+        expr.left = expr.left.id;
+        expr.right = expr.right.id;
+        return result;
+    }
+    default: {
+        console.error(`Undefined expression type ${expr.type}.`);
+        return [expr];
+    }
+    }
+}
+
 export function reduct(state=initialState, act) {
     switch (act.type) {
     case action.START_LEVEL: {
+        let nodes = [];
+        let goal = [];
+        let board = [];
+        let toolbox = [];
+        for (const expr of act.goal) {
+            nodes = nodes.concat(flatten(expr));
+            goal.push(expr.id);
+        }
+        for (const expr of act.board) {
+            nodes = nodes.concat(flatten(expr));
+            board.push(expr.id);
+        }
+        for (const expr of act.toolbox) {
+            nodes = nodes.concat(flatten(expr));
+            toolbox.push(expr.id);
+        }
         return Object.assign({}, state, {
-            goal: act.goal,
-            board: act.board,
-            toolbox: act.toolbox,
+            nodes: nodes,
+            goal: goal,
+            board: board,
+            toolbox: toolbox,
         });
     }
     default: {
