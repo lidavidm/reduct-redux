c4ec5bb3c27b3bcb83d1195777195cd447fce2f4
David Li <li.davidm96@gmail.com>
2017-12-11 14:49:57 -0500

    Implement a "three-patch" sprite container
    Nine-patches are sprites that scale based on being subdividable
    into 9 images; this "three-patch" is similar, but only scales
    in one direction.

--- src/gfx/sprite.js
+++ src/gfx/sprite.js
@@ -27,3 +27,64 @@ export function sprite(options={}) {
     };
     return projection;
 }
+
+export function patch3(childFunc, options={}) {
+    const projection = baseProjection();
+    projection.type = "3patch";
+
+    projection.prepare = function(id, state, stage) {
+        const childId = childFunc(id, state);
+        const childProjection = stage.views[childId];
+        childProjection.prepare(childId, state, stage);
+        projection.middleSegments = Math.ceil(childProjection.size.w / options.middle.naturalWidth);
+        projection.imageScale = 1.4 * childProjection.size.h / options.middle.naturalHeight;
+        const middleWidth = projection.middleSegments * projection.imageScale * options.middle.naturalWidth;
+        childProjection.pos.x = options.left.naturalWidth * projection.imageScale + (middleWidth - childProjection.size.w) / 2;
+        childProjection.pos.y = (options.middle.naturalHeight * projection.imageScale - childProjection.size.h) / 2;
+    };
+    projection.draw = function(id, state, stage, offset) {
+        const ctx = stage.ctx;
+        ctx.save();
+
+        let [ sx, sy ] = util.absoluteScale(projection, offset);
+        sx *= projection.imageScale;
+        sy *= projection.imageScale;
+
+        if (projection.opacity) ctx.globalAlpha = projection.opacity;
+
+        const topY = offset.y + projection.pos.y * offset.sy;
+
+        options.left.draw(ctx,
+                          offset.x + projection.pos.x * offset.sx,
+                          topY,
+                          sx * options.left.naturalWidth,
+                          sy * options.left.naturalHeight);
+
+        let x = offset.x + projection.pos.x * offset.sx + sx * options.left.naturalWidth;
+        let subX = x;
+
+        for (let i = 0; i < projection.middleSegments; i++) {
+            const w = sx * options.middle.naturalWidth;
+            options.middle.draw(ctx, x, topY, w, sy * options.middle.naturalHeight);
+            x += w;
+        }
+
+        options.right.draw(ctx, x, topY,
+                           sx * options.right.naturalWidth,
+                           sy * options.right.naturalHeight);
+
+        const childId = childFunc(id, state);
+        const subOffset = Object.assign({}, offset, {
+            x: offset.x + projection.pos.x * offset.sx,
+            y: offset.y + projection.pos.y * offset.sy,
+            sx: offset.sx * projection.scale.x,
+            sy: offset.sy * projection.scale.y,
+        });
+        stage.views[childId].draw(childId, state, stage, subOffset);
+
+        debugDraw(ctx, projection, offset);
+
+        ctx.restore();
+    };
+    return projection;
+}
