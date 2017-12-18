e371c381e280dfce7d846bb14c8033f21ee707c6
David Li <li.davidm96@gmail.com>
2017-12-18 10:23:30 -0700

    Initial data-driven definition of semantics

---
+++ src/semantics/es6.js
@@ -0,0 +1,94 @@
+import transform from "./transform";
+
+export default transform({
+    name: "ECMAScript 6",
+    parser: null,
+
+    expressions: {
+        number: {
+            fields: ["value"],
+            subexpressions: [],
+            projection: {
+                type: "default",
+                shape: "()",
+                color: "white",
+            },
+        },
+
+        op: {
+            fields: ["name"],
+            subexpressions: [],
+            projection: {
+                type: "text",
+                text: "{name}",
+            },
+        },
+
+        binop: {
+            fields: [],
+            subexpressions: ["left", "op", "right"],
+            projection: {
+                type: "default",
+                shape: "()",
+                color: "orange",
+            },
+            smallStep: (semant, expr) => semant.number(expr.left.value + expr.right.value),
+        },
+
+        lambda: {
+            fields: [],
+            subexpressions: ["arg", "body"],
+            projection: {
+                type: "default",
+                shape: "()",
+            },
+            betaReduce: (semant, expr, args) => null,
+        },
+
+        lambdaArg: {
+            fields: ["name"],
+            subexpressions: [],
+            projection: {
+                type: "text",
+                text: "({name})",
+            },
+        },
+
+        lambdaVar: {
+            fields: ["name"],
+            subexpressions: [],
+            projection: {
+                type: "default",
+                shape: "()",
+                strokeWhenChild: false,
+            },
+        },
+
+        symbol: {
+            fields: ["name"],
+            subexpressions: [],
+            projection: {
+                type: "case",
+                on: "name",
+                cases: {
+                    star: {
+                        type: "symbol",
+                        symbol: "star",
+                    },
+                    circle: {
+                        type: "symbol",
+                        symbol: "circle",
+                    },
+                    triangle: {
+                        type: "symbol",
+                        symbol: "triangle",
+                    },
+                    rect: {
+                        type: "symbol",
+                        symbol: "rect",
+                    },
+                }
+            },
+        }
+    },
+});
