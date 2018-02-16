adb43db0691a5c1f2d79db8e5fff46d545f19cab
David Li <li.davidm96@gmail.com>
2018-02-16 17:57:18 -0500

    Have arrow color reflect argument presence

--- src/gfx/decal.js
+++ src/gfx/decal.js
@@ -15,17 +15,22 @@ export default function decal(projection) {
         const lastChild = { x: 0, y: 0 };
         let first = true;
 
+        let firstFilled = false;
+        let lastFilled = false;
+
         const nodes = state.get("nodes");
         for (const [ childId, subexprId ] of this.children(exprId, state)) {
             const view = stage.views[childId];
+            const subexpr = nodes.get(subexprId);
             if (first) {
                 first = false;
+                firstFilled = subexpr.get("type") !== "missing";
                 firstChild.x = view.pos.x;
                 firstChild.y = view.pos.y;
             }
 
-            const subexpr = nodes.get(subexprId);
             if (subexpr && subexpr.get("parentField") === "argument") {
+                lastFilled = subexpr.get("type") !== "missing";
                 lastChild.x = view.pos.x + (view.size.w / 2);
                 lastChild.y = view.pos.y;
             }
@@ -43,10 +48,12 @@ export default function decal(projection) {
             x + (sx * lastChild.x), y + (sy * lastChild.y),
             x + (sx * firstChild.x), y + (sy * firstChild.y)
         );
-        gradient.addColorStop(0, "#14818b");
-        gradient.addColorStop(0.7, "#c500ff");
-        gradient.addColorStop(1, "#ff004b");
+        gradient.addColorStop(0, lastFilled ? "#14818b" : "gray");
+        gradient.addColorStop(0.7, (firstFilled && lastFilled) ? "#c500ff" : "gray");
+        gradient.addColorStop(1, firstFilled ? "#ff004b" : "gray");
         ctx.fillStyle = gradient;
+
+        if (this.opacity) ctx.globalAlpha = this.opacity;
 
         const cx = x + (sx * ((lastChild.x - firstChild.x) / 2));
         const arrowBase = 5;
