e4b68376121fed307bfd1ba09af504db0afc9342
David Li <li.davidm96@gmail.com>
2018-02-24 11:12:02 -0500

    Draw title on chapter end screen

--- src/stage/chapterend.js
+++ src/stage/chapterend.js
@@ -1,3 +1,5 @@
+import * as gfx from "../gfx/core";
+
 import BaseStage from "./basestage";
 
 export default class ChapterEndStage extends BaseStage {
@@ -5,5 +7,28 @@ export default class ChapterEndStage extends BaseStage {
         super(...args);
 
         this.color = "#594764";
+
+        const title = gfx.layout.sticky(gfx.text("Chapter Finished!", {
+            fontSize: 40,
+            color: "#FFF",
+        }), "top", {
+            align: "center",
+            margin: 50,
+        });
+        this.title = this.internalViews[this.allocateInternal(title)];
+
+        this.draw();
+    }
+
+    drawContents() {
+        const state = this.getState();
+        this.title.prepare(null, null, state, this);
+        this.title.draw(null, null, state, this, {
+            x: 0,
+            y: 0,
+            sx: 1,
+            sy: 1,
+            opacity: 1,
+        });
     }
 }
