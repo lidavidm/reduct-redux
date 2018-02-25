d245d7f70a47319fd0ade98c1ea044b670011ed6
David Li <li.davidm96@gmail.com>
2018-02-25 11:45:07 -0500

    Draw alien based on images specified in chapter
    Fixes #88.

--- src/ui/goal.js
+++ src/ui/goal.js
@@ -1,9 +1,22 @@
 import * as gfx from "../gfx/core";
+import * as progression from "../game/progression";
 import Loader from "../loader";
 
 export default class Goal {
     constructor(stage) {
         this.stage = stage;
+
+        const chapter = progression.currentChapter();
+        const alienIndex = (progression.currentLevel() - chapter.startIdx)
+              % chapter.resources.aliens.length;
+        const image = Loader.images[chapter.resources.aliens[alienIndex]];
+        const alien = stage.allocate(gfx.sprite({
+            image,
+            size: { h: 80, w: 80 * (image.frame.w / image.frame.h) },
+        }));
+
+        this.alien = alien;
+
         const container = stage.allocate(gfx.layout.hbox((_id, state) => {
             return state.get("goal");
         }, {
@@ -30,10 +43,34 @@ export default class Goal {
     }
 
     drawImpl(state) {
+        const alien = this.stage.views[this.alien];
+        alien.prepare(null, null, state, this.stage);
+
+        const { ctx } = this.stage;
+        ctx.save();
+        ctx.beginPath();
+        ctx.fillStyle = "#000";
+        ctx.moveTo(0, 18 + alien.size.h);
+        ctx.quadraticCurveTo(18 + alien.size.w, 18 + alien.size.h, 15 + alien.size.w, 0);
+        ctx.lineTo(0, 0);
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.fillStyle = "#594764";
+        ctx.moveTo(0, 15 + alien.size.h);
+        ctx.quadraticCurveTo(15 + alien.size.w, 15 + alien.size.h, 15 + alien.size.w, 0);
+        ctx.lineTo(0, 0);
+        ctx.fill();
+        ctx.restore();
+
+        alien.draw(null, null, state, this.stage, {
+            x: 0, y: 0, sx: 1, sy: 1,
+        });
+
         const view = this.stage.views[this.textGoal ? this.textGoal : this.container];
         view.prepare(null, null, state, this.stage);
         view.draw(null, null, state, this.stage, {
-            x: 0, y: 0, sx: 1, sy: 1,
+            x: alien.size.w, y: 5, sx: 1, sy: 1,
         });
     }
 
