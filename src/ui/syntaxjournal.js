8d2c8a9f3bb6d803f6e380738f005a7df916a859
David Li <li.davidm96@gmail.com>
2018-02-26 16:45:01 -0500

    Render learned syntaxes
    Fixes #90.

--- src/ui/syntaxjournal.js
+++ src/ui/syntaxjournal.js
@@ -1,5 +1,6 @@
 import * as gfx from "../gfx/core";
 import * as animate from "../gfx/animate";
+import * as progression from "../game/progression";
 import Loader from "../loader";
 
 export default class SyntaxJournal {
@@ -20,12 +21,41 @@ export default class SyntaxJournal {
         }), "center"));
 
         this.state = "closed";
+
+        this.syntaxes = {};
     }
 
     drawImpl(state) {
         if (this.isOpen) {
             this.stage.drawInternalProjection(state, this.overlay);
             this.stage.drawInternalProjection(state, this.background);
+
+            const bg = this.stage.getView(this.background);
+            let y = bg.pos.y + 40;
+            for (const syntax of progression.getLearnedSyntaxes()) {
+                if (!this.syntaxes[syntax]) {
+                    const image = Loader.images[syntax];
+                    const sprite = gfx.sprite({
+                        image,
+                        size: { w: image.naturalWidth, h: image.naturalHeight },
+                    });
+                    sprite.anchor = { x: 0.5, y: 0 };
+                    this.syntaxes[syntax] = this.stage.allocateInternal(sprite);
+                }
+
+                const view = this.stage.getView(this.syntaxes[syntax]);
+                view.pos.x = this.stage.width / 2;
+                view.pos.y = y;
+                y += view.size.h + 10;
+
+                this.stage.drawInternalProjection(state, this.syntaxes[syntax], null, {
+                    x: 0,
+                    y: 0,
+                    sx: 1,
+                    sy: 1,
+                    opacity: bg.opacity,
+                });
+            }
         }
     }
 
