2c6adb9fc5e0614892ac0b88017f3c1759d88afe
David Li <li.davidm96@gmail.com>
2018-04-25 21:06:44 -0400

    Add next/redo buttons

--- src/ui/navbar.js
+++ src/ui/navbar.js
@@ -18,6 +18,17 @@ export default class Navbar {
             size: { w: 60, h: 60 },
         }));
 
+        this.next = stage.allocate(gfx.ui.imageButton({
+            normal: Loader.images["btn-next-default"],
+            hover: Loader.images["btn-next-hover"],
+            active: Loader.images["btn-next-down"],
+        }, {
+            click: () => {
+                window.next();
+            },
+            size: { w: 60, h: 60 },
+        }));
+
         this.undo = stage.allocate(gfx.ui.imageButton({
             normal: Loader.images["btn-back-default"],
             hover: Loader.images["btn-back-hover"],
@@ -30,17 +41,31 @@ export default class Navbar {
             size: { w: 60, h: 60 },
         }));
 
-        this.buttons = [ this.reset, this.undo ];
+        this.redo = stage.allocate(gfx.ui.imageButton({
+            normal: Loader.images["btn-next-default"],
+            hover: Loader.images["btn-next-hover"],
+            active: Loader.images["btn-next-down"],
+        }, {
+            click: () => {
+                this.stage.store.dispatch(undo.redo());
+            },
+            size: { w: 60, h: 60 },
+        }));
+
+        this.buttons = [ this.reset, this.next, this.undo, this.redo ];
+
+        const topButtons = [ this.reset, this.next ];
+        const bottomButtons = [ this.undo, this.redo ];
 
         const topRow = stage.allocate(gfx.layout.hbox(
-            () => [ this.reset ],
+            () => topButtons,
             {
                 subexpScale: 1,
             },
             gfx.baseProjection
         ));
         const bottomRow = stage.allocate(gfx.layout.hbox(
-            () => [ this.undo ],
+            () => bottomButtons,
             {
                 subexpScale: 1,
             },
